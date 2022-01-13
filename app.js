b882da452c09d728fda9e0992ee0cb8fa5075293
Keisi Polli <keisi.polli@khk.ee>
2022-01-13 13:37:39 +0200

    Added item to the UI list

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const ItemCtrl = (function(){
     // Data Structure
     const data = {
         items: [
-            {id: 0, name: "Steak Dinner", calories: 1200},
-            {id: 1, name: "Cookie", calories: 400},
-            {id: 2, name: "Eggs", calories: 300}
+           //{id: 0, name: "Steak Dinner", calories: 1200},
+           // {id: 1, name: "Cookie", calories: 400},
+           // {id: 2, name: "Eggs", calories: 300}
         ],
         total: 0
     }
@@ -81,6 +81,23 @@ const UICtrl = (function(){
                 name:document.querySelector(UISelectors.itemNameInput).value,
                 calories:document.querySelector(UISelectors.itemCaloriesInput).value
             }
+        },
+        addListItem: function (item){
+            // create li element
+            const li = document.createElement("li");
+            // add class
+            li.className = "collection-item";
+            // add ID
+            li.id = `item ${item.id}`;
+            // add HTML
+            li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em> <a href="#" class="secondary-content"> <i class="edit-item fa fa-pencil"></i></a>`;
+            //insert item
+            document.querySelector(UISelectors.itemList).insertAdjacentElement("beforeend", li)
+
+        },
+        clearInput: function (){
+            document.querySelector(UISelectors.itemNameInput).value = "";
+            document.querySelector(UISelectors.itemCaloriesInput).value = "";
         }
         }
 })();
@@ -101,6 +118,9 @@ const App = (function(ItemCtrl, UICtrl){
         // check for name and calorie input
         if(input.name !== "" && input.calories !== ""){
             const newItem = ItemCtrl.addItem(input.name, input.calories)
+            UICtrl.addListItem(newItem)
+            // clear fields
+            UICtrl.clearInput();
         }
 
         event.preventDefault()
